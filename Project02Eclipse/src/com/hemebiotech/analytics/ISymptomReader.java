a2557657f3fc26e6963b99d996d4d01f4c869936
Antoine-CJ-Thomas <antoine.cj.thomas@gmail.com>
2020-09-25 12:29:43 +0200

    Adding JavaDoc

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -11,8 +11,9 @@ import java.util.List;
  * 
  */
 public interface ISymptomReader {
+
 	/**
-	 * If no data is available, return an empty List
+	 * Read all data of a file then return it in a list (if no data is available, return an empty List)
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
 	 *         are possible/probable
--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -2,7 +2,17 @@ package com.hemebiotech.analytics;
 
 import java.util.TreeMap;
 
+/**
+ * The wrote result will not contain duplications of the same symptom
+ * 
+ * The result must be ordered alphabetically
+ */
 public interface ISymptomWriter {
-
+	
+	/**
+	 * Write all symptoms name and their value in a file
+	 * 
+	 * @param symptomTable contain the type and the value of each symptom
+	 */
 	void writeSymptoms(TreeMap<String, Integer> symptomTable);
 }
